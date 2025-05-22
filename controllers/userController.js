a66585e483ac28ee90322abe37acc82f7788a43d
Pranjal <prathammishra947@gmail.com>
2025-05-22 17:16:55 +0530

    Code to get user details by randomInteger

--- controllers/userController.js
+++ controllers/userController.js
@@ -106,6 +106,22 @@ const signInWithGoogle = async (req, res) => {
   };
   
 
+// Get details of a user by randomInteger
+const getUserByRandomInteger = async (req, res) => {
+    const { randomInteger } = req.params;
+
+    try {
+        const user = await User.findOne({ randomInteger });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error fetching user:', error.message);
+        res.status(500).json({ message: 'Server error' });
+    }
+}  
+
 // Update user Details 
 const updateUserDetails = async (req, res) => {
     const { name, email, profilePictureUrl, randomInteger } = req.body;
@@ -136,10 +152,13 @@ const updateUserDetails = async (req, res) => {
 
 
 
+
+
 module.exports = { 
     signInOrSignUpUser,
     getAllUsers ,
     signInWithGoogle,
-    updateUserDetails
+    updateUserDetails,
+    getUserByRandomInteger
     };
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { signInOrSignUpUser, getAllUsers, signInWithGoogle, updateUserDetails } = require('../controllers/userController');
+const { signInOrSignUpUser, getAllUsers, signInWithGoogle, updateUserDetails, getUserByRandomInteger } = require('../controllers/userController');
 
 
 router.post('/', signInOrSignUpUser);
 router.get('/', getAllUsers);
 router.post('/google', signInWithGoogle);
 router.put('/update', updateUserDetails);
+router.get('/getuserdetails', getUserByRandomInteger);
 
 
 
