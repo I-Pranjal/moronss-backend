d1e24b9bf1567f74a8f4b3bdebe46b417f29a560
Pranjal <prathammishra947@gmail.com>
2025-06-10 03:31:58 +0530

    puppeteer issue

--- utils/analyseWebsite.js
+++ utils/analyseWebsite.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 async function analysePortfolio(url) {
-  const browser = await puppeteer.launch(); // headless by default
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: 'networkidle2' });
 
